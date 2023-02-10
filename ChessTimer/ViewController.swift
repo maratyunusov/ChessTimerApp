c4fee238bbc2bd89daef9711d5b5d400dd2901ed
Marat <m.z.yunusov@gmail.com>
2023-02-10 17:52:15 +0400

    Created project, add gitignore file.

---
+++ ChessTimer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ChessTimer
+//
+//  Created by Marat Yunusov on 10.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
